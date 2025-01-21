8bd55915266ea4f96d3cd2a538ecc6eea1b63c53
kapplegate2020 <111217483+kapplegate2020@users.noreply.github.com>
2025-01-20 21:21:55 -0700

    Added toString for piece and board, and added the reset board function.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Arrays;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -8,6 +10,26 @@ package chess;
  */
 public class ChessBoard {
     private ChessPiece[][] squares = new ChessPiece[8][8];
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        for(int i=8; i>=1; i--){
+            output.append("|");
+            for(int j=1;j<=8;j++){
+                ChessPiece current = getPiece(new ChessPosition(i,j));
+                if(current == null){
+                    output.append(" ");
+                }
+                else {
+                    output.append(current);
+                }
+                output.append("|");
+            }
+            output.append("\n");
+        }
+        return output.toString();
+    }
 
     public ChessBoard() {
         
@@ -39,6 +61,47 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        ChessGame.TeamColor w = ChessGame.TeamColor.WHITE;
+        ChessGame.TeamColor b = ChessGame.TeamColor.BLACK;
+
+        //first row
+        addPiece(new ChessPosition(1,1), new ChessPiece(w, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(1,2), new ChessPiece(w, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1,3), new ChessPiece(w, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1,4), new ChessPiece(w, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(1,5), new ChessPiece(w, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(1,6), new ChessPiece(w, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1,7), new ChessPiece(w, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1,8), new ChessPiece(w, ChessPiece.PieceType.ROOK));
+
+        //white pawns in second row
+        for(int i=1;i<=8;i++){
+            addPiece(new ChessPosition(2,i), new ChessPiece(w, ChessPiece.PieceType.PAWN));
+        }
+
+        //middle all blank
+        for(int i=3; i<=8; i++){
+            for(int j=1; j<=8; j++){
+                addPiece(new ChessPosition(i,j), null);
+            }
+        }
+
+        //black pawns in second to last row
+        for(int i=1;i<=8;i++){
+            addPiece(new ChessPosition(7,i), new ChessPiece(b, ChessPiece.PieceType.PAWN));
+        }
+
+        //last row
+        addPiece(new ChessPosition(8,1), new ChessPiece(b, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(8,2), new ChessPiece(b, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8,3), new ChessPiece(b, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8,4), new ChessPiece(b, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(8,5), new ChessPiece(b, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(8,6), new ChessPiece(b, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8,7), new ChessPiece(b, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8,8), new ChessPiece(b, ChessPiece.PieceType.ROOK));
+
+
+
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -30,6 +30,51 @@ public class ChessPiece {
         PAWN
     }
 
+    @Override
+    public String toString() {
+        if(pieceColor == ChessGame.TeamColor.WHITE){
+            if(type == PieceType.KING){
+                return "K";
+            }
+            else if(type == PieceType.QUEEN){
+                return "Q";
+            }
+            else if(type == PieceType.ROOK){
+                return "R";
+            }
+            else if(type == PieceType.KNIGHT){
+                return "N";
+            }
+            else if(type == PieceType.BISHOP){
+                return "B";
+            }
+            else if(type == PieceType.PAWN){
+                return "P";
+            }
+        }
+        if(pieceColor == ChessGame.TeamColor.BLACK){
+            if(type == PieceType.KING){
+                return "k";
+            }
+            else if(type == PieceType.QUEEN){
+                return "q";
+            }
+            else if(type == PieceType.ROOK){
+                return "r";
+            }
+            else if(type == PieceType.KNIGHT){
+                return "n";
+            }
+            else if(type == PieceType.BISHOP){
+                return "b";
+            }
+            else if(type == PieceType.PAWN){
+                return "p";
+            }
+        }
+        return "ERROR";
+    }
+
     /**
      * @return Which team this chess piece belongs to
      */
