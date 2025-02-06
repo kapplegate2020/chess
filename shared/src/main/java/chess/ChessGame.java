b8f2db4d78ffd9ffbd9ad3592386adf676022b3a
kapplegate2020 <111217483+kapplegate2020@users.noreply.github.com>
2025-02-06 09:59:14 -0700

    Implemented isInCheckMate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -195,7 +195,26 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        if(!isInCheck(teamColor)){
+            return false;
+        }
+
+        boolean hasValidMove = false;
+        for(int i=1; i<=8; i++){
+            for(int j=1; j<=8; j++){
+                ChessPosition position = new ChessPosition(i, j);
+                ChessPiece piece = board.getPiece(position);
+                if(piece != null){
+                    if(piece.getTeamColor()== teamColor){
+                        if(!validMoves(position).isEmpty()){
+                            hasValidMove = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return !hasValidMove;
     }
 
     /**
