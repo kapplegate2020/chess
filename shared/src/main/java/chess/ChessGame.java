227361f7164c26ea28a9031247b8493eaf1dfdaf
kapplegate2020 <111217483+kapplegate2020@users.noreply.github.com>
2025-02-07 00:52:06 -0700

    caught little bug

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -50,7 +50,7 @@ public class ChessGame {
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         ChessPiece piece = board.getPiece(startPosition);
-        if(board == null){
+        if(piece == null){
             return null;
         }
 
