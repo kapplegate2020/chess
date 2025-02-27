5f64c4d74af4956465d8d85f0f1d42020a3b8245
kapplegate2020 <111217483+kapplegate2020@users.noreply.github.com>
2025-02-27 00:05:15 -0700

    creates the memory game data access

--- server/src/main/java/dataAccess/GameDataAccess.java
+++ server/src/main/java/dataAccess/GameDataAccess.java
@@ -5,9 +5,9 @@ import model.GameData;
 import java.util.ArrayList;
 
 public interface GameDataAccess {
-    GameData createGame(GameData gameData) throws DataAccessException;
+    void createGame(GameData gameData) throws DataAccessException;
     ArrayList<GameData> listGames() throws DataAccessException;
-    GameData updateGame(GameData gameData) throws DataAccessException;
-    GameData getGame(String gameID) throws DataAccessException;
+    void updateGame(GameData gameData) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
     void clear() throws DataAccessException;
 }
---
+++ server/src/main/java/dataAccess/MemoryGameDataAccess.java
@@ -0,0 +1,43 @@
+package dataAccess;
+
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MemoryGameDataAccess implements GameDataAccess{
+    HashMap<Integer, GameData> games = new HashMap<Integer, GameData>();
+
+    @Override
+    public void createGame(GameData gameData) throws DataAccessException {
+        if(games.containsKey(gameData.gameID())){
+            throw new DataAccessException("GameID is already taken.");
+        }
+    }
+
+    @Override
+    public ArrayList<GameData> listGames() throws DataAccessException {
+        return new ArrayList<GameData>(games.values());
+    }
+
+    @Override
+    public void updateGame(GameData gameData) throws DataAccessException {
+        if(!games.containsKey(gameData.gameID())){
+            throw new DataAccessException("GameID does not exist.");
+        }
+        games.put(gameData.gameID(), gameData);
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        if(games.containsKey(gameID)){
+            return games.get(gameID);
+        }
+        return null;
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        games.clear();
+    }
+}
