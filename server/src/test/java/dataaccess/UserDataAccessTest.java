b26f6383fddf5a68a3fa4c7bb9a94b68f81ce04a
kapplegate2020 <111217483+kapplegate2020@users.noreply.github.com>
2025-03-10 21:26:56 -0600

    wrote success test for get user

--- server/src/test/java/dataaccess/UserDataAccessTest.java
+++ server/src/test/java/dataaccess/UserDataAccessTest.java
@@ -18,6 +18,27 @@ public class UserDataAccessTest {
     }
 
     @Test
+    public void getUserSuccess(){
+        try (var conn = DatabaseManager.getConnection()) {
+            String statement = "INSERT INTO user (username, password, email) VALUES ('Andy', 'passw0rd', 'fake@mail.com')";
+            try (var insertPreparedStatement = conn.prepareStatement(statement)) {
+                insertPreparedStatement.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try{
+            UserData userData = userDataAccess.getUser("Andy");
+            assert userData.username().equals("Andy");
+            assert userData.password().equals("passw0rd");
+            assert userData.email().equals("fake@mail.com");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
     public void createUserSuccess(){
         UserData userData = new UserData("Adam", "secure12", "email@email.com");
         try {
