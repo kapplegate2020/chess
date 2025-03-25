75c008c9ca7399241be7d28a22132c71ef02237a
Kenny Applegate <kapplegate2020@gmail.com>
2025-03-25 02:43:29 -0600

    implemented some loggedinClient methods.

--- client/src/main/java/ui/LoggedInClient.java
+++ client/src/main/java/ui/LoggedInClient.java
@@ -1,16 +1,22 @@
 package ui;
 
 import client.ResponseException;
+import client.ServerFacade;
+import request.CreateGameRequest;
+import request.LogoutRequest;
+import result.CreateGameResult;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class LoggedInClient implements Client{
-    String serverURL;
+    ServerFacade serverFacade;
     Repl repl;
     String authToken;
+    Map<Integer, Integer> gameIDs = null;
 
     public LoggedInClient(String serverURL, Repl repl, String authToken){
-        this.serverURL = serverURL;
+        serverFacade = new ServerFacade(serverURL);
         this.repl = repl;
         this.authToken = authToken;
     }
@@ -25,7 +31,7 @@ public class LoggedInClient implements Client{
         String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
         return switch (command) {
             case "logout" -> logout(params);
-            case "create" -> listGames(params);
+            case "create" -> createGame(params);
             case "list" -> listGames(params);
             case "join" -> join(params);
             case "observe" -> observe(params);
@@ -44,11 +50,24 @@ public class LoggedInClient implements Client{
                 """;
     }
 
-    private logout(String[] params){
+    private String logout(String[] params) throws ResponseException{
         if(params.length!=0){
             return "Invalid Command.";
         }
+        LogoutRequest logoutRequest = new LogoutRequest(authToken);
+        serverFacade.logout(logoutRequest);
         repl.logout();
         return "";
     }
+
+    private String createGame(String[] params) throws  ResponseException{
+        if(params.length != 1){
+            return "Invalid Command.";
+        }
+        CreateGameRequest createGameRequest= new CreateGameRequest(authToken, params[0]);
+        serverFacade.createGame(createGameRequest);
+        return "Successfully created game "+params[0];
+    }
+
+
 }
