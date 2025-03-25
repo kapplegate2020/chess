4bcfe4c5e3920bfc78fea0df74f17fbce55a5984
Kenny Applegate <kapplegate2020@gmail.com>
2025-03-24 22:02:31 -0600

    wrote successful test for register

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,38 @@
+package client;
+
+import com.google.gson.Gson;
+import request.RegisterRequest;
+import result.RegisterResult;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+
+public class ServerFacade {
+    String url;
+    public ServerFacade(String url){
+        this.url = url;
+    }
+
+    public RegisterResult register(RegisterRequest registerRequest) throws Exception{
+        HttpURLConnection http = (HttpURLConnection) new URI(url+"/user").toURL().openConnection();
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        http.addRequestProperty("Content-Type", "application/json");
+        String reqData = new Gson().toJson(registerRequest);
+        try (OutputStream reqBody = http.getOutputStream()){
+            reqBody.write(reqData.getBytes());
+        }
+        http.connect();
+        int statusCode = http.getResponseCode();
+        RegisterResult registerResult;
+        try (InputStream respBody = http.getInputStream()) {
+            InputStreamReader inputStreamReader = new InputStreamReader(respBody);
+            registerResult = new Gson().fromJson(inputStreamReader, RegisterResult.class);
+        }
+        return registerResult;
+    }
+
+}
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,25 @@
 package client;
 
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.*;
+import request.RegisterRequest;
+import result.RegisterResult;
 import server.Server;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade("http://localhost:"+port);
     }
 
     @AfterAll
@@ -22,8 +29,15 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerSuccess() {
+        RegisterRequest registerRequest = new RegisterRequest("Jimmy", "test1234", "me@fake.com");
+        try{
+            RegisterResult registerResult = serverFacade.register(registerRequest);
+            assert registerResult.username().equals("Jimmy");
+            assert registerResult.authToken() != null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
