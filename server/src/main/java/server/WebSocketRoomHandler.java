c3210a90a9cc2dd485b4f87d4fd30b4a9f50feb4
Kenny Applegate <kapplegate2020@gmail.com>
2025-04-10 01:45:03 -0600

    implemented broadcast

--- server/src/main/java/server/WebSocketRoomHandler.java
+++ server/src/main/java/server/WebSocketRoomHandler.java
@@ -1,6 +1,8 @@
 package server;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
 
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,5 +19,23 @@ public class WebSocketRoomHandler {
 
     public void remove(Integer gameId, Session session){
         rooms.get(gameId).remove(session);
+    }
+
+    public void broadcast(Integer gameId, Session excludedSession, ServerMessage serverMessage) throws Exception{
+        ArrayList<Session> removeList = new ArrayList<>();
+        for(Session session : rooms.get(gameId)){
+            if(session.isOpen()){
+                if(session!=excludedSession){
+                    session.getRemote().sendString(new Gson().toJson(serverMessage));
+                }
+            }
+            else{
+                removeList.add(session);
+            }
+        }
+
+        for(Session session : removeList){
+            rooms.get(gameId).remove(session);
+        }
     }
 }
